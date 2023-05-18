10d6b4571baae0f301473ff0398303c156cdf2b6
vWernay <victorlwernay@gmail.com>
2023-05-18 15:06:17 -0300

    ✨ server(back-end): authentication + routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,22 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
+import { authRotes } from './routes/auth'
 
 const app = fastify()
 
 app.register(cors, {
   origin: true,
 })
+
+app.register(jwt, {
+  secret: '43766b67d79d3e167f89a2cf6d0465a2', // 16 bytes (128 bits)
+})
+
+app.register(authRotes)
 app.register(memoriesRoutes)
 
 app
