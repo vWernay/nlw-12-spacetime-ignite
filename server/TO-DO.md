a799c76a145eb4218be057af8eb835762446e98b
vWernay <victorlwernay@gmail.com>
2023-05-21 18:57:28 -0300

    ✨ feat (back-end): Image uploading (static storage)

---
+++ server/TO-DO.md
@@ -0,0 +1,2 @@
+# Image Server
+- [ ] Change image server to a cloud service (Amazon, Google, Cloudflare) instead of local server.
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,10 +3,21 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
+import fastifyStatic from '@fastify/static'
 import { memoriesRoutes } from './routes/memories'
-import { authRotes } from './routes/auth'
+import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(fastifyStatic, {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
@@ -16,7 +27,8 @@ app.register(jwt, {
   secret: '43766b67d79d3e167f89a2cf6d0465a2', // 16 bytes (128 bits)
 })
 
-app.register(authRotes)
+app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
